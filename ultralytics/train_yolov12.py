03d9127e8bb725e5283eb3e5d0522111c2b0f44e
Kilimanjaro_codeworld <liukunjsj@163.com>
2025-10-17 09:51:41 +0800

    将yolov12_fuse文件放入目录中

--- ultralytics/train_yolov12.py
+++ ultralytics/train_yolov12.py
@@ -14,12 +14,12 @@ warnings.filterwarnings("ignore", category=UserWarning, module="albumentations.c
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 #  Load a COCO-pretrained YOLO12n model
-model = YOLO(r"D:\Git\yolov12_fuse_SA\ultralytics\cfg\models\v12\yolov12_fuse.yaml")
+model = YOLO(r"./yolov12_fuse.yaml")
 # data=r"F:\my_code\yolov12_fuse_SA\ultralytics\cfg\datasets\TransmissionTower.yaml",
 #D:\Git\yolov12_fuse_SA\ultralytics\cfg\models\v10\yolov10m_fuse_ndsi.yaml
 # Train the model on the COCO8 example dataset for 100 epochs
 results = model.train(data=r'./TransmissionTower.yaml',
-                      epochs=1,
+                      epochs=50,
                       patience=10,
                       imgsz=416,
                       workers=0,
